78c4887fcd3368970d163ad756214d7c3148b4bd
Sam Case <samcase@Sams-MacBook-Air-2.local>
2020-08-29 19:11:34 -0500

    Updates

---
+++ precache-manifest.69b0e052498d9f18506641ea1e034337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8d4c2130af5f57795423ad2ae4ea9b",
+    "url": "/bm-game/index.html"
+  },
+  {
+    "revision": "6c13c5a436de00fbfc33",
+    "url": "/bm-game/static/css/main.0218f248.chunk.css"
+  },
+  {
+    "revision": "d37e96fb47647224c988",
+    "url": "/bm-game/static/js/2.a1741d5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bm-game/static/js/2.a1741d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c13c5a436de00fbfc33",
+    "url": "/bm-game/static/js/main.f0cef27e.chunk.js"
+  },
+  {
+    "revision": "21ca750564b9cb478175",
+    "url": "/bm-game/static/js/runtime-main.f5dd30e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bm-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
